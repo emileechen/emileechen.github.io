b8f9e915bc97706bd48fdc170130ff275ca76948
Emilee Chen <emilee.ty.chen@gmail.com>
2016-05-01 23:38:51 -0700

    hover works now bless u arlan

--- portraits/script.js
+++ portraits/script.js
@@ -1,24 +1,8 @@
 
 
-
 window.onload = function() {
 	initializeImages();
 };
-
-
-
-function unhover(element) {
-	var url = image.getAttribute('src');
-	var new_url = "p" + url.slice(1);
-    image.setAttribute('src', new_url);
-    console.log(image.getAttribute('src'));
-}
-
-function hover(image) {
-	var url = (image.getAttribute('src'));
-	var new_url = "s" + url.slice(1);
-    image.setAttribute('src', new_url);
-}
 
 function pad(n) {
     return n > 9 ? "" + n: "0" + n;
@@ -29,12 +13,21 @@ function initializeImages() {
 	var container = document.getElementById("grid");
 
 	for (i = 1; i <= n; i++) {
-		image = document.createElement("img");
-		image.setAttribute('class', 'pic');
-		image.setAttribute('id', pad(i));
-		image.setAttribute('src', "p/" + pad(i) + ".jpg");
-		image.setAttribute('onmouseout', "unhover(this)");
-		image.setAttribute('onmouseover', "hover(this)");
-		container.appendChild(image);
+		piccontain = document.createElement("div");
+		piccontain.setAttribute('class', 'pic-contain');
+
+		s = document.createElement('img');
+		s.setAttribute('class', 'pic');
+		s.setAttribute('src', "s/" + pad(i) + ".jpg");
+
+		p = document.createElement('img');
+		p.setAttribute('class', 'pic hide');
+		p.setAttribute('src', "p/" + pad(i) + ".jpg");
+
+		piccontain.appendChild(s);
+		piccontain.appendChild(p);
+		container.appendChild(piccontain);
 	}
 }
+
+
