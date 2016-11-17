513aa2986b9b26e266887eb202734a72bbce524f
Emilee Chen <emilee.ty.chen@gmail.com>
2016-11-16 22:55:43 -0800

    formating for project page sidebar nav (back, up)

--- js/magicstuff.js
+++ js/magicstuff.js
@@ -1,52 +1,13 @@
-// document.getElementById()
-
-
-// var currLocation = 0;
-// var currPosition = document.documentElement.scrollTop || document.body.scrollTop;
-
-
-var pages = ['splash', 'about', 'projects', 'contact'];
-
-var pageLocations = {};
-function refreshLocations() {
-	for (p = 0; p < pages.length; p++) {
-		pageLocations[pages[p]] = document.getElementById(pages[p]).offsetTop;
-	}
-}
-
 
 // Unhide #go-to-top when past the splash page.
 
 $(window).scroll(function() {
-	var start = 100;
-	if ($(this).scrollTop() > start) {
-	    $("#go-to-top:hidden").css('visibility', 'visible');
-	    $("#go-to-top:hidden").fadeIn('slow');
+	var scroll = $(window).scrollTop();
+
+	if (scroll > $(window).height()/2) {
+	    $(".side-nav").removeClass("hidden");
 	}
 	else {
-		$("#go-to-top:visible").fadeOut('slow');
+	    $(".side-nav").addClass("hidden");
 	}
 });
-
-
-
-
-// $(window).on("scroll", function(e) {
-//  	var scrollTop = $(window).scrollTop();
-//  	refreshLocations();
-// 	for (p = 1; p < pages.length; p++) {
-// 		console.log(pages[p]);
-//  		if (scrollTop > pageLocations[pages[p]]) {
-// 			$('#' + pages[p] + '-title').addClass("box-content-top");
-//  		} else {
-// 			$('#' + pages[p] + '-title').removeClass("box-content-top");
-//  		}
-// 	}
-
-// 	// if (scrollTop > pageLocations.projects) {
-// 	// 	$('#project-title').addClass("box-content-top");
-// 	// }
-// 	//  else {
-// 	// 	$('#project-title').removeClass("box-content-top");
-// 	// }
-// });
